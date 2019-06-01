cf48076933c162f4a254e68ee97129cf5e486b1a
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2019-05-31 22:08:12 -0700

    Yaml loading

--- build.sbt
+++ build.sbt
@@ -6,5 +6,8 @@ name := "kamu-core-manifests"
 version := "0.1.0-SNAPSHOT"
 
 libraryDependencies ++= Seq(
-  "org.apache.hadoop" % "hadoop-common" % "2.6.5" // % "provided"
+  "org.apache.hadoop" % "hadoop-common" % "2.6.5" % "provided",
+  // Config
+  "com.github.pureconfig" %% "pureconfig" % "0.10.2" % "provided",
+  "com.github.pureconfig" %% "pureconfig-yaml" % "0.10.2" % "provided"
 )
--- src/main/scala/dev/kamu/core/manifests/DataSourcePolling.scala
+++ src/main/scala/dev/kamu/core/manifests/DataSourcePolling.scala
@@ -1,13 +1,12 @@
 package dev.kamu.core.manifests
 
+import java.io.InputStream
 import java.net.URI
-import org.apache.hadoop.fs.Path
 
-object DataSourcePolling {
-  val DEFAULT_READER_OPTIONS: Map[String, String] = Map(
-    "mode" -> "FAILFAST"
-  )
-}
+import org.apache.hadoop.fs.Path
+import pureconfig.generic.ProductHint
+import pureconfig.module.yaml.loadYamlOrThrow
+import pureconfig.{CamelCase, ConfigFieldMapping, ConfigReader}
 
 case class DataSourcePolling(
   /** Unique identifier of the dataset */
@@ -42,6 +41,46 @@ case class DataSourcePolling(
   def withDefaults(): DataSourcePolling = {
     copy(
       readerOptions =
-        DataSourcePolling.DEFAULT_READER_OPTIONS ++ readerOptions)
+        DataSourcePolling.DEFAULT_READER_OPTIONS ++ readerOptions
+    )
+  }
+}
+
+object DataSourcePolling {
+  import pureconfig.generic.auto._
+
+  val DEFAULT_READER_OPTIONS: Map[String, String] = Map(
+    "mode" -> "FAILFAST"
+  )
+
+  implicit val pathReader = ConfigReader[String]
+    .map(s => new Path(URI.create(s)))
+
+  implicit def hint[T]: ProductHint[T] =
+    ProductHint[T](
+      ConfigFieldMapping(CamelCase, CamelCase),
+      useDefaultArgs = true,
+      allowUnknownKeys = false
+    )
+
+  def load(inputStream: InputStream): DataSourcePolling = {
+    val dataString = scala.io.Source.fromInputStream(inputStream).mkString
+    load(dataString)
+  }
+
+  def load(dataString: String): DataSourcePolling = {
+    val value = loadYamlOrThrow[DataSourcePolling](dataString)
+    value.withDefaults()
+  }
+
+  def loadManifest(inputStream: InputStream): Manifest[DataSourcePolling] = {
+    val dataString = scala.io.Source.fromInputStream(inputStream).mkString
+    loadManifest(dataString)
+  }
+
+  def loadManifest(dataString: String): Manifest[DataSourcePolling] = {
+    val value = loadYamlOrThrow[Manifest[DataSourcePolling]](dataString)
+    // TODO: Validate the manifest version/kind
+    value.copy(content = value.content.withDefaults())
   }
 }
