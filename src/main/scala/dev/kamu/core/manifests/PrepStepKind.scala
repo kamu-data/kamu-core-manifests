51646ddabeda2c4516b4b41646b5585530209eae
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2019-11-10 20:22:48 -0800

    Add pipe prep step

--- src/main/scala/dev/kamu/core/manifests/PrepStepKind.scala
+++ src/main/scala/dev/kamu/core/manifests/PrepStepKind.scala
@@ -4,6 +4,7 @@ import org.apache.hadoop.fs.Path
 
 sealed trait PrepStepKind
 
+/** Decompresses the archive file */
 case class PrepStepDecompress(
   /** Name of a compression algorithm used on data */
   format: String,
@@ -11,4 +12,10 @@ case class PrepStepDecompress(
   subPath: Option[Path] = None,
   /** Regex for finding desired data file within a multi-file archive */
   subPathRegex: Option[String] = None
+) extends PrepStepKind
+
+/** Executes external command to process the data using piped input/output */
+case class PrepStepPipe(
+  /** Command to execute and its arguments */
+  command: Vector[String]
 ) extends PrepStepKind
